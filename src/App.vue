04a5659d68586343c24ad3ecf09232a593db993e
Elias Lövdinger <elias.lovdinger@edu.varberg.se>
2023-05-24 12:42:46 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div style="align-items: center; justify-content: center">
+    <h1>This is my start site</h1>
+    <br />
+    <p>You are going to authenticate here on this site</p>
+  </div>
+</template>
+
+<style scoped></style>
